09ddac38a0bff6224b27f12b1f980f402bfbc776
chandu038 <chandudarapaneni093@gmail.com>
2024-08-25 13:22:55 +0530

    some button work need to complete

--- src/Pages/Signup/SignupForm.jsx
+++ src/Pages/Signup/SignupForm.jsx
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth ,firestore } from "../../firebase/firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import './SignupForm.css'
 
 function Signup() {
   const navi=  useNavigate();
@@ -56,118 +57,49 @@ await setDoc(docRef, {
     }
   };
   return (
-    <Container maxWidth="sm">
-      <Card
-        sx={{ mt: 8, p: 4, border: "1px solid #ccc", borderRadius: 2 }}
-        elevation={10}
-      >
-        <Button onClick={loginpage}
-            type="submit"
-            variant="contained"
-            sx={{
-              backgroundColor: "#efefef",
-              color: "black",
-              borderRadius: "50px",
-              mt: 2,
-            }}
-            fullWidth
-          >
-             Back top login Page
-          </Button>
-        
-        <Typography variant="h3" gutterBottom>
-          Signup
-        </Typography>
-        <form onSubmit={handleSignup}>
-          <FormField
-            label="Name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <FormField
-            label="VTU-Email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <FormField
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <FormField
-            label="VTU-No"
-            type="text"
-            value={vtuno}
-            onChange={(e) => setVtuno(e.target.value)}
-          />
-          <FormField
-            label="Phone"
-            type="tel" // Changed to tel for phone numbers
-            value={studentPhone}
-            onChange={(e) => setStudentPhone(e.target.value)}
-          />
-          <FormField
-            label="Department"
-            type="text"
-            value={department}
-            onChange={(e) => setDepartment(e.target.value)}
-          />
-          <FormField
-            label="Year"
-            type="number"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-          />
-          <FormField
-            label="DOB"
-            type="date"
-            value={dob}
-            onChange={(e) => setDob(e.target.value)}
-          />
-          <FormField
-            label="Mentor Name"
-            type="text"
-            value={mentorName}
-            onChange={(e) => setMentorName(e.target.value)}
-          />
-          <FormField
-            label="Mentor Phone"
-            type="tel" // Changed to tel for phone numbers
-            value={mentorPhone}
-            onChange={(e) => setMentorPhone(e.target.value)}
-          />
-
-          <Button
-            type="submit"
-            variant="contained"
-            sx={{
-              backgroundColor: "#efefef",
-              color: "black",
-              borderRadius: "50px",
-              mt: 2,
-            }}
-            fullWidth
-          >
-            Sign Up
-          </Button>
-
-          {error && (
-            <Typography color="error" sx={{ mt: 2 }}>
-              {error}
-            </Typography>
-          )}
-          {success && (
-            <Typography color="success" sx={{ mt: 2 }}>
-              {success}
-            </Typography>
-          )}
-        </form>
-      </Card>
-    </Container>
-  );
+  <>
+<div className="signup-box">
+<h1 className="title">Create Your Account</h1>
+<form>
+  <label htmlFor="Name" id="label">Name</label><br/>
+  <input type="text" title="Name"  placeholder="Enter your name"className="enter-name"/><br />
+  <label htmlFor="Email" id="mail">Email</label><br/>
+  <input type="Email" title="Email"  placeholder="Enter your vtu mail"className="mail"/> <br />
+  <label htmlFor="Vtu-no" id="vtu">Vtu Num</label> <br />
+  <input type="text"title="vtu number" placeholder="Enter your vtu number" className="vtu-number"/> <br />
+  <label htmlFor="password" id="word">Password</label> <br />
+  <input type="password"title="password"  placeholder="Enter Your password" className="word"/> <br />
+  <label htmlFor="Mentor" id="mentor">Mentor Name</label><br />
+  <input type="text" tittle="mentor-name" placeholder="Enter Your mentor name" className="mentor"/><br />
+  <label htmlFor="Mentor" id="mentor-phnum">Mentor Phone</label><br />
+  <input type="text" tittle="mentor-number" placeholder="Enter Your mentor phone number" className="mentor-phnum"/><br />
+  <label htmlFor="Dob" id="stud-dob">Student-DOB</label><br />
+  <input type="date" tittle="mentor-number" placeholder="Date of Birth" className="stud-dob"/><br />
+  <div>
+    <label htmlFor="Year of study" id="yos"> Year of Studying:</label><br />
+        <select className="yos" name="Year of Study">
+            <option value="null"> Year of study</option>
+            <option> 2year </option>
+            <option>3year</option>
+            <option>4year</option>
+        </select>
+    </div>
+    <div>
+    <label htmlFor="Branch" id="branch"> Studying Branch:</label><br />
+        <select className="branch" name="Studying Branch">
+            <option>CSE </option>
+            <option>AIDS</option>
+            <option>AIML</option>
+            <option>CYBER SECURITY(CS)</option>
+            <option>CSE-AIDS</option>
+            <option>CSE-AIML</option>
+            <option>CSE-CS</option>
+        </select>
+    </div>
+    <button>Register</button>
+</form>
+</div>
+  </>);
 }
 
 export default Signup;
